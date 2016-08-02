f237c82c08b376821023e0c310a2a5ea90d3a941
brbeardm <bbeardmore@carvizor.com>
2016-08-01 19:09:00 -0500

    Initial commit

---
+++ LCDTester.py
@@ -0,0 +1,30 @@
+import LCDDisplay
+import time
+import Queue as queue
+
+Parameters = {'mode': 'Off', 'target': 225, 'P': .01, 'I': 0, 'D': 3.0, 'AugerOnTime': 15, 'AugerOffTime': 65}
+Temps = [[1, 2, 3], [4, 5, 6]]
+
+#Initialize LCD
+qP = queue.Queue()
+qT = queue.Queue()
+qR = queue.Queue()
+
+qP.put(Parameters)
+qT.put(Temps)
+
+lcd = LCDDisplay.LCDDisplay(qP, qT, qR)
+lcd.setDaemon(True)
+lcd.start()
+
+i = 1
+while True:
+    qT.put([[i, i, i], [i, i, i]])
+
+    while not qR.empty():
+        NewParameters = qR.get()
+        for k in NewParameters:
+            Parameters[k] = NewParameters[k]
+        qP.put(Parameters)
+    i += 1
+    time.sleep(0.25)
